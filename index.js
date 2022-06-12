c6b75e2fa1126a1ec875120e04c9ec5e1b64231e
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-06-13 02:32:27 +0600

    post method

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Running my node project')
 })
 
-const user = [
+const users = [
     {id: 1, name: 'shakil', email: 'sakil@gmail.com'},
     {id: 2, name: 'shakil2', email: 'sakil@gmai2.com'},
     {id: 3, name: 'shakil3', email: 'sakil@gmai3.com'}
 ]
 
-app.get('/user', (req, res)=>{
-    res.send(user)
+app.get('/users', (req, res)=>{
+    res.send(users)
+})
+
+app.get('/user/:id', (req, res)=>{
+  const id= parseInt(req.params.id);
+  const user = users.find(u=>u.id===id)
+  res.send(user);
+})
+
+app.post('/user', (req, res)=>{
+  console.log(req.body);
+  console.log('post method success')
 })
 
 app.listen(port, () => {
