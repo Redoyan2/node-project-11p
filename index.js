eb3da2cd38a219b3c6e82e26f69edea28b2d810f
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-06-15 05:16:52 +0600

    post and get

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
@@ -7,32 +8,54 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+//username: assignment11
+//password: cvuSR0ILOJz5O97S
+
+
+
+const uri = "mongodb+srv://assignment11:cvuSR0ILOJz5O97S@cluster0.3wot6ap.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+  try {
+    await client.connect();
+    const bookCollection = client.db("bookExpress").collection('user')
+
+
+    app.post('/users', async(req, res) =>{
+      const newUser = req.body;
+      const result = await bookCollection.insertOne(newUser);
+      res.send(result)
+  });
+
+  app.get('/users', async(req, res) =>{
+    const query = {};
+    const cursor = bookCollection.find(query);
+    const users = await cursor.toArray();
+    res.send(users);
+});
+
+
+  
+
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
   res.send('Running my node project')
 })
 
-const users = [
-    {id: 1, name: 'shakil', email: 'sakil@gmail.com'},
-    {id: 2, name: 'shakil2', email: 'sakil@gmai2.com'},
-    {id: 3, name: 'shakil3', email: 'sakil@gmai3.com'}
-]
 
-app.get('/users', (req, res)=>{
-    res.send(users)
-})
 
-app.get('/user/:id', (req, res)=>{
-  const id= parseInt(req.params.id);
-  const user = users.find(u=>u.id===id)
-  res.send(user);
-})
 
-app.post('/user', (req, res)=>{
-  const user = req.body;
-  user.id = users.length+1;
-  users.push(user)
-  res.send(user);
-})
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port`, port)
